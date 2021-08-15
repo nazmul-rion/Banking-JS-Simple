360c08090009e77d63031b4328a66ec4c60f0593
nazmul-rion <nazmulrion16@gmail.com>
2021-08-15 21:29:52 +0600

    1st commit

--- js/home.js
+++ js/home.js
@@ -5,7 +5,7 @@ const loginbtn = document.getElementById('login-button');
 loginbtn.addEventListener('click', function () {
     const email = emailField.value;
     const pass = passField.value;
-    if (email == "nazmul@gmail.com" && pass == "rion") {
+    if (email == "nazmul" && pass == "rion") {
         window.location.href = "bank.html";
     }
 })
